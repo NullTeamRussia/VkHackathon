7428bb638b431595bc299e912c2d38544ff2adf8
Nikita Ivanov <6de1ay@gmail.com>
2017-10-20 20:23:57 +0300

    Added: icon menu

--- frontend/src/components/HermitagePanelMain.vue
+++ frontend/src/components/HermitagePanelMain.vue
@@ -2,19 +2,30 @@
   <div id="hermitage_panel_main">
       <div class="sidebar">
         <div class="brand">
-            <span class="brand-text">Hermitage</span>
         </div>
         <div class="sidebar__menu">
             <div class="menu__item"
-                @click="active_page = 'hemitage_panel_main'"
-                :class="{'menu__item--active': active_page === 'hermitage_panel_main'}">
+                @click="active_page = 'hermitage_panel_dashboard'"
+                :class="{'menu__item--active': active_page === 'hermitage_panel_dashboard'}">
+                <icon name="dashboard" scale="1" class="menu__item-icon"></icon>
+            </div>
+            <div class="menu__item"
+                @click="active_page = 'hermitage_panel_nodes'"
+                :class="{'menu__item--active': active_page === 'hermitage_panel_nodes'}">
                 <icon name="map-o" scale="1" class="menu__item-icon"></icon>    
-                <span class="menu__item-text">Павильоны</span>
+            </div>
+             <div class="menu__item"
+                @click="active_page = 'hermitage_panel_exponats'"
+                :class="{'menu__item--active': active_page === 'hermitage_panel_exponats'}">
+                <icon name="picture-o" scale="1" class="menu__item-icon"></icon>
             </div>
         </div>   
       </div>
       <div class="container">
           <div class="header">
+              <div class="header-page">
+                  <span class="header-page__text">{{page_names[active_page]}}</span>
+              </div>
           </div>
           <!-- <component :is="active_page"></component> -->
       </div>
@@ -23,11 +34,18 @@
 
 <script>
     import 'vue-awesome/icons/map-o'
+    import 'vue-awesome/icons/picture-o'
+    import 'vue-awesome/icons/dashboard'
     export default {
       name: 'HermitagePanelMain',
       data () {
         return {
-          active_page: 'hermitage_panel_main'
+          page_names: {
+            'hermitage_panel_dashboard': 'статистика',
+            'hermitage_panel_nodes': 'павильоны',
+            'hermitage_panel_exponats': 'экспонаты'
+          },
+          active_page: 'hermitage_panel_dashboard'
         }
       }
     }
@@ -50,9 +68,9 @@
         flex-direction: column;
         justify-content: flex-start;
         align-items: stretch;
-        width: 200px;
+        width: 50px;
         background: #fff;
-        box-shadow: 0.5px 0 0 0.5px rgba(0, 0, 0, 0.3); 
+        box-shadow: 0.5px 0 1px 0 rgba(50, 50, 50, 0.3);
     }
 
     .container {
@@ -66,13 +84,7 @@
         align-items: center;
         margin-bottom: 50px;
         height: 50px;
-        box-shadow: 0 0.5px 0 0.5px rgba(0, 0, 0, 0.3);
-    }
-
-    .brand-text {
-        color: #2ecc71;
-        font-size: 32px;
-        font-weight: 800;
+        box-shadow: 0 0.5px 1px 0 rgba(50, 50, 50, 0.3);
     }
 
     .menu__item {
@@ -80,26 +92,16 @@
         flex-direction: row;
         justify-content: center;
         align-items: center;
-        margin-left: 5%;
         height: 50px;
-        width: 95%;
         color: #2ecc71;
         transition: all 0.3s ease-in-out;
     }
 
     .menu__item:hover { cursor: pointer; }
-    .menu__item:not(.menu__item--active) { 
+
+    .menu__item:not(.menu__item--active):hover { 
         background: #2ecc71;
         color: #fff;
-    }
-
-    .menu__item-icon {
-        margin-top: 3px;
-        margin-right: 7px;
-    }
-
-    .menu__item-text {
-        font-size: 24px;
     }
 
     .menu__item--active {
@@ -112,8 +114,38 @@
         content: '';
         background: #27ae60;
         height: 50px;
-        width: 10px;
-        left: 190px;
+        width: 5px;
+        left: 45px;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: stretch;
+    }
+
+    .header {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: stretch;
+        height: 50px;
+        box-shadow: 0 0.5px 1px 0 rgba(50, 50, 50, 0.3);
+    }
+
+    .header-page {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-left: 20px;
+    }
+
+    .header-page__text {
+        text-transform: capitalize;
+        font-size: 24px;
+        color: #2ecc71;
     }
 
 </style>
