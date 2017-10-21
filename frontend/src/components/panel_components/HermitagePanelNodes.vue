f2a0eed1595268c5d21f67029805b8092b7f9ca0
Nikita Ivanov <6de1ay@gmail.com>
2017-10-22 02:58:17 +0300

    Added: edges

--- frontend/src/components/panel_components/HermitagePanelNodes.vue
+++ frontend/src/components/panel_components/HermitagePanelNodes.vue
@@ -35,8 +35,15 @@
             scale="1.4">
           </icon>
         </div>
+        <div @click="activateLinkMode" v-if="current_point.top"
+        :class="{'node__action--active': canvas_active_mode === 'nodes_linking'}">
+          <icon 
+            name="link" 
+            scale="1.4">
+          </icon>
+        </div>
       </div>
-      <div class="node_info" v-if="!is_node_adding_panel_active">
+      <div class="node_info" v-if="!is_node_adding_panel_active && canvas_active_mode !== 'nodes_linking'">
         <div class="node_info-name">
           <span class="node_info-name__text">{{current_node.name}}</span>
         </div>
@@ -54,7 +61,7 @@
           <span class="node_info-available__text" v-if="!current_node.available">Недоступен для посещения</span>
         </div>
       </div>
-      <div class="node_info node_info--new" v-if="is_node_adding_panel_active">
+      <div class="node_info node_info--new" v-if="is_node_adding_panel_active && canvas_active_mode !==  'nodes_linking'">
         <div class="node_info-name">
           <input class="node_info-name__text" placeholder="Название..." type="text" v-model="new_node.name"/>
         </div>
@@ -76,6 +83,32 @@
           <span class="node_info-coordinates__text">Поставить точку</span>
         </div>
       </div>
+      <div class="node_info nodes_linking" v-if="!is_node_adding_panel_active && canvas_active_mode === 'nodes_linking'">
+        <div class="node_info-name">
+          <span class="node_info-name__text">{{first_linking_node.name}}</span>
+        </div>
+        <div class="node_info-max">
+          <span class="node_info-max__text">Вместимость</span>
+          <span class="node_info-max__value">{{first_linking_node.maxCount}}</span>
+        </div>
+        <div class="node_info-space">
+        </div>
+        <div class="node_info-name">
+          <span class="node_info-name__text">{{second_linking_node.name}}</span>
+        </div>
+        <div class="node_info-max">
+          <span class="node_info-max__text">Вместимость</span>
+          <span class="node_info-max__value">{{second_linking_node.maxCount}}</span>
+        </div>
+        <div class="node_info-space">
+        </div>
+         <div class="node_info-weight">
+          <span class="node_info-weight__text">Время</span>
+          <input class="node_info-weight__value" v-model="linking_nodes_weight"/>
+        </div>
+        <div class="nodes_linking-submit" @click="linkNodes"
+        :class="{'submit--blocked': second_linking_node.name === 'Выберите 2-ую точку'}">Соединить павильоны</div>
+      </div>
     </div>
   </div>
 </template>
@@ -88,6 +121,7 @@
     import 'vue-awesome/icons/search-plus'
     import 'vue-awesome/icons/arrows'
     import 'vue-awesome/icons/trash'
+    import 'vue-awesome/icons/link'
     export default {
       name: 'HermitagePanelNodes',
       props: [
@@ -105,6 +139,7 @@
           canvas_moving: false,
           canvas_moving_allowed: true,
           canvas_active_mode: '',
+          current_floor: 1,
           current_point: {},
           current_node: {
             name: '',
@@ -123,7 +158,26 @@
             exponats: [],
             x: 0,
             y: 0
-          }
+          },
+          first_linking_node: {
+            name: 'Выберите 1-ую точку',
+            available: true,
+            maxCount: 0,
+            description: '',
+            exponats: [],
+            x: 0,
+            y: 0
+          },
+          second_linking_node: {
+            name: 'Выберите 2-ую точку',
+            available: true,
+            maxCount: 0,
+            description: '',
+            exponats: [],
+            x: 0,
+            y: 0
+          },
+          linking_nodes_weight: 0
         }
       },
       mounted () {
@@ -137,21 +191,25 @@
         )
         this.nodes.forEach(node => {
           var newNode = new fabric.Circle({
-            selectable: true
+            selectable: true,
+            hoverCursor: 'pointer'
           })
           newNode.setPositionByOrigin(new fabric.Point(node.x, node.y))
           newNode.setRadius(5)
           newNode.setColor('#e74c3c')
-          newNode.node = node
           this.nodes_graphics.add(newNode)
+        })
+        this.edges.forEach(edge => {
+          var newLine = new fabric.Line([edge.startX, edge.startY, edge.endX, edge.endY], {
+            stroke: '#e74c3c'
+          })
+          this.nodes_graphics.add(newLine)
         })
         this.nodes_graphics.setBackgroundImage('/static/hermitage_first_stage.jpg', this.nodes_graphics.renderAll.bind(this.nodes_graphics), {
           originX: 'left',
           originY: 'top'
         })
         this.activateViewMode()
-        this.nodes_graphics.on('object:selected', this.nodeSelected)
-        this.nodes_graphics.on('selection:cleared', this.resetCurrentNode)
       },
       methods: {
         activateViewMode () {
@@ -163,6 +221,8 @@
             this.nodes_graphics.on('mouse:up', this.stopMoveCanvas)
             this.nodes_graphics.on('mouse:down', this.startMoveCanvas)
             this.nodes_graphics.on('mouse:move', this.moveCanvas)
+            this.nodes_graphics.on('object:selected', this.nodeSelected)
+            this.nodes_graphics.on('selection:cleared', this.resetCurrentNode)
           }
         },
         activateNodeAddingPanel () {
@@ -179,7 +239,8 @@
         addNode (e) {
           if (this.canvas_active_mode === 'node_adding') {
             var newNode = new fabric.Circle({
-              selectable: false
+              selectable: false,
+              hoverCursor: 'pointer'
             })
             var point = this.nodes_graphics.getPointer(e.e)
             newNode.setPositionByOrigin(new fabric.Point(point.x, point.y))
@@ -254,6 +315,68 @@
             this.$emit('update:nodes', this.nodes)
           })
           this.resetCurrentNode()
+        },
+        activateLinkMode () {
+          this.canvas_active_mode = 'nodes_linking'
+          this.nodes_graphics.off('object:selected', this.nodeSelected)
+          this.nodes_graphics.on('object:selected', this.selectSecondLinkingNode)
+          this.first_linking_node = this.current_node
+          this.nodes_graphics.off('selection:cleared', this.resetCurrentNode)
+        },
+        resetLinkingNodes () {
+          this.first_linking_node = {
+            name: 'Выберите 1-ую точку',
+            available: true,
+            maxCount: 0,
+            description: '',
+            exponats: [],
+            x: 0,
+            y: 0
+          }
+          this.second_linking_node = {
+            name: 'Выберите 2-ую точку',
+            available: true,
+            maxCount: 0,
+            description: '',
+            exponats: [],
+            x: 0,
+            y: 0
+          }
+          this.linking_nodes_weight = 0
+        },
+        selectSecondLinkingNode (e) {
+          this.second_linking_node = this.nodes.filter(node => {
+            return node.x === e.target.left + 0.5 && node.y === e.target.top + 0.5
+          })[0]
+          this.nodes_graphics.off('object:selected', this.selectSecondLinkingNode)
+        },
+        linkNodes () {
+          if (this.second_linking_node.name === 'Выберите 2-ую точку') {
+            return
+          }
+          var line = new fabric.Line(
+            [
+              this.first_linking_node.x + 0.5,
+              this.first_linking_node.y + 0.5,
+              this.second_linking_node.x + 0.5,
+              this.second_linking_node.y + 0.5
+            ], {
+              stroke: '#e74c3c'
+            }
+          )
+          this.nodes_graphics.add(line)
+          axios.post('http://api.hermitage.nullteam.info/edges', {
+            startX: this.first_linking_node.x + 0.5,
+            startY: this.first_linking_node.y + 0.5,
+            endX: this.second_linking_node.x + 0.5,
+            endY: this.second_linking_node.y + 0.5,
+            weight: this.linking_nodes_weight
+          }).then(resp => {
+            this.edges.push(resp.data)
+            this.$emit('update:edges', this.edges)
+          })
+          this.resetLinkingNodes()
+          this.activateViewMode()
         }
       }
     }
@@ -283,17 +406,19 @@
     justify-content: flex-start;
     align-items: stretch;
     min-width: 300px;
+    max-width: 300px;
   }
 
   .node__actions {
     display: flex;
     flex-direction: row;
-    justify-content: flex-start;
+    justify-content: space-between;
     align-items: center;
     margin-bottom: 50px;
     min-height: 50px;
     background: #fff;
     box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 4px 0 rgba(0,0,0,.12);
+    transition: all 0.3s ease-in-out;
   }
 
   .node__actions > div {
@@ -301,7 +426,7 @@
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    padding: 0 15px;
+    width: 100%;
     height: 50px;
     color: #3498db;
     border-left: 0.5px solid rgba(0, 0, 0, 0.3);
@@ -310,7 +435,6 @@
   }
 
   .node__actions > div:first-child {
-    padding-left: 15px;
     border: none; 
   }
 
@@ -322,6 +446,7 @@
   .node__actions .node__action--active {
     color: #fff;
     background: #2980b9;
+    border: none;
   }
 
   .node_info {
@@ -339,15 +464,19 @@
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
-    height: 50px;
+    min-height: 50px;
     color: #fff; 
     background: #3498db;
   }
 
   .node_info-name__text {
     margin: 0 10px;
-    font-size: 24px
+    font-size: 24px;
+    white-space: nowrap;
+    overflow: auto;
   }
+
+  .node_info-name__text::-webkit-scrollbar { width: 0; height: 0; }
 
   .node_info-description {
     flex-grow: 1;
@@ -356,6 +485,7 @@
     justify-content: flex-start;
     align-items: stretch;
     border-bottom: 0.5px solid rgba(0, 0, 0, 0.3);
+    overflow: hidden;
   }
 
   .node_info-description__text {
@@ -363,10 +493,11 @@
     width: 100%;
     font-size: 16px;
     font-family: 'Source Sans Pro', 'Open Sans', 'sans-selif';
+    overflow: auto;
   }
 
   .node_info-max {
-    height: 50px;
+    min-height: 50px;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -387,7 +518,7 @@
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    height: 50px;
+    min-height: 50px;
   }
 
   .node_info-available--available {
@@ -429,7 +560,7 @@
   }
 
   .node_info-coordinates {
-    height: 50px;
+    min-height: 50px;
     display: flex;
     flex-direction: row;
     justify-content: center;
@@ -438,6 +569,51 @@
     color: #fff;
     font-size: 18px;
     cursor: pointer;
+  }
+
+  .nodes_linking-submit {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    min-height: 50px;
+    font-size: 18px;
+    background: #3498db;
+    color: #fff;
+    cursor: pointer;
+  }
+
+  .node_info-weight {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    min-height: 50px;
+    border-top: 0.5px solid rgba(0, 0, 0, 0.3);
+  }
+
+  .node_info-weight__text { 
+    margin-left: 10px; 
+    white-space: nowrap;
+  }
+
+  .node_info-weight__value {
+    margin-right: 10px;
+    border: none;
+    text-align: right;
+    outline: none;
+    font-weight: 600;
+    font-size: 16px;
+  }
+
+  .node_info-space {
+    min-height: 50px;
+    border-top: 0.5px solid rgba(0, 0, 0, 0.3);
+    background: #fff; 
+  }
+
+  .submit--blocked:hover {
+    cursor: not-allowed;
   }
 
 </style>
